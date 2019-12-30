4618b4d57ef0a510278fee33ebe10a255b173771
Aftab Alam <mdaftabalam014@yahoo.com>
2019-12-31 03:08:47 +0530

    Add files via upload

---
+++ app/src/main/java/com/mdaftabalam/onboarding/MainActivity.java
@@ -0,0 +1,63 @@
+package com.mdaftabalam.onboarding;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.ViewPropertyAnimatorCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Here we are setting animation delay time
+    public static final int STARTUP_DELAY = 300;
+    public static final int ANIM_ITEM_DURATION = 1000;
+    public static final int EDITTEXT_DELAY = 300;
+    public static final int BUTTON_DELAY = 500;
+    public static final int VIEW_DELAY = 400;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView logoImageView = findViewById(R.id.img_logo);
+        ViewGroup container = findViewById(R.id.container);
+
+        //Code for app logo animation
+        ViewCompat.animate(logoImageView)
+                .translationY(-250)
+                .setStartDelay(STARTUP_DELAY)
+                .setDuration(ANIM_ITEM_DURATION).setInterpolator(
+                new DecelerateInterpolator(1.2f)).start();
+
+        //Here we are setting animation on displaying content
+        for (int i = 0; i < container.getChildCount(); i++) {
+            View v = container.getChildAt(i);
+            ViewPropertyAnimatorCompat viewAnimator;
+
+            if (v instanceof EditText) {
+                viewAnimator = ViewCompat.animate(v)
+                        .scaleY(1).scaleX(1)
+                        .setStartDelay((EDITTEXT_DELAY * i) + 500)
+                        .setDuration(500);
+            } else if (v instanceof Button) {
+                viewAnimator = ViewCompat.animate(v)
+                        .scaleY(1).scaleX(1)
+                        .setStartDelay((BUTTON_DELAY * i) + 500)
+                        .setDuration(500);
+            } else {
+                viewAnimator = ViewCompat.animate(v)
+                        .translationY(50).alpha(1)
+                        .setStartDelay((VIEW_DELAY * i) + 500)
+                        .setDuration(1000);
+            }
+            viewAnimator.setInterpolator(new DecelerateInterpolator()).start();
+        }
+    }
+}
